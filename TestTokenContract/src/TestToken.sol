d0375d28c28c674904f3dd5f9fcf669315c6a2b1
hasanza <hr90@student.london.ac.uk>
2023-06-20 22:33:27 +0500

    setup TestVault

--- TestTokenContract/src/TestToken.sol
+++ TestTokenContract/src/TestToken.sol
@@ -1,75 +1,53 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface IERC20 {
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-    function balanceOf(address account) external view returns (uint256);
-    function approve(address spender, uint256 amount) external returns (bool);
-}
+import "openzeppelin-contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-contracts/access/Ownable.sol";
 
-contract TestVault {
-    address public owner;
+contract TestVault is Ownable, ERC20 {
     uint public rate;
-    IERC20 public testToken;
 
     event TokensPurchased(address indexed buyer, uint amount, uint cost);
     event TokensSold(address indexed seller, uint amount, uint revenue);
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-    event RateSet(uint previousRate, uint newRate);
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the contract owner can call this function");
-        _;
-    }
+    event RateSet(uint indexed previousRate, uint indexed newRate);
 
     /**
-    Sets the exchange rate, deploys TestToken, makes deployer the owner
+    @notice wSets the exchange rate, deploys TestToken, makes deployer the owner
      */
-    constructor(uint _rate) {
-        require(_rate > 0, "Rate must be greater than zero");
-
-        owner = msg.sender;
+    constructor(uint _rate) ERC20("TestToken", "TTK") {
+        require(_rate > 0, "TestVault: Rate must be greater than zero");
+        // The rate defines how much ETH is required to be deposited for 1 TTK
         rate = _rate;
-// Deploy test token
     }
 
     function buyTokens() external payable {
+        require(msg.value > 0, "TestVault: Insufficient ETH provided");
         uint tokenAmount = msg.value * rate;
-        require(tokenAmount > 0, "Insufficient ETH provided");
-
-        testToken.transfer(msg.sender, tokenAmount);
-
+        require(tokenAmount > 0, "TestVault: Invalid token amount");
+        // Transfer to caller
+        transfer(msg.sender, tokenAmount);
+    
         emit TokensPurchased(msg.sender, tokenAmount, msg.value);
     }
 
     function sellTokens(uint amount) external {
-        require(amount > 0, "Invalid token amount");
+        require(amount > 0, "TestVault: Invalid token amount");
 
         uint ethAmount = amount / rate;
-        require(ethAmount > 0, "Insufficient token amount");
+        require(ethAmount > 0, "TestVault: Insufficient token amount");
 
-        require(testToken.balanceOf(msg.sender) >= amount, "Insufficient token balance");
+        require(balanceOf(msg.sender) >= amount, "TestVault: Insufficient token balance");
 
-        testToken.transferFrom(msg.sender, address(this), amount);
+        transferFrom(msg.sender, address(this), amount);
 
         (bool success, ) = payable(msg.sender).call{value: ethAmount}("");
-        require(success, "Failed to send ETH to seller");
+        require(success, "TestVault: Failed to send ETH to seller");
 
         emit TokensSold(msg.sender, amount, ethAmount);
     }
 
-    function transferOwnership(address newOwner) external onlyOwner {
-        require(newOwner != address(0), "Invalid new owner address");
-
-        address previousOwner = owner;
-        owner = newOwner;
-
-        emit OwnershipTransferred(previousOwner, newOwner);
-    }
-
     function setRate(uint newRate) external onlyOwner {
-        require(newRate > 0, "Rate must be greater than zero");
+        require(newRate > 0, "TestVault: Rate must be greater than zero");
 
         uint previousRate = rate;
         rate = newRate;
