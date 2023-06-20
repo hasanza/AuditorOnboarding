04cc88fc1b08e70dd467c4e5eb3cf7c25c9a81b8
hasanza <hr90@student.london.ac.uk>
2023-06-21 01:12:11 +0500

    test setup

--- TestTokenContract/test/TestVault.t.sol
+++ TestTokenContract/test/TestVault.t.sol
@@ -4,4 +4,14 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/TestVault.sol";
 
+contract VaultTest is Test {
+    // Variables
 
+    // Setup
+       function setUp() public {
+     
+    }
+
+    // Tests
+
+}
